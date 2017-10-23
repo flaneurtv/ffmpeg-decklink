750bfabc2220dc5e3b9b8798c7c00731f9e1396c
Markus Kienast <mark@trickkiste.at>
2017-10-23 23:31:46 +0200

    typo

--- Library/Formula/decklink.rb
+++ Library/Formula/decklink.rb
@@ -1,6 +1,6 @@
 class Decklink < Formula
   homepage "https://github.com/flaneurtv/ffmpeg-decklink"
-  url "https://github.com/flaneurtv/ffmpeg-decklink/raw/master/decklink-api-10.9,5.tar.gz"
+  url "https://github.com/flaneurtv/ffmpeg-decklink/raw/master/decklink-api-10.9.5.tar.gz"
   sha256 "4d275b806e7e4d96818f3c3aa49b6c1e99049b095e768050cbf4212c418cf540"
 
   def install
